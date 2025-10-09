21cb7e3517a9203a2b6c285c51504c89e3eb7471
Manuel Antonio Arias Osuna <sonantonio@gmail.com>
2025-10-09 15:26:04 -0700

    Actualizar

--- subs/cnx_db.php
+++ subs/cnx_db.php
@@ -1,13 +1,13 @@
 <?php
 require_once('funcionesmysql.php');
 //Conexion con la base
-if (!$MySQL=@mysql_connect('localhost', 'vereficentros', 'bAllenA#66#')) {
+if (!$MySQL=@mysql_connect('localhost', 'vereficentros', 'bAllenA6##6')) {
 	$t=time();
 	while (time()<$t+5) {}
-	if (!$MySQL=@mysql_connect('localhost', 'vereficentros', 'bAllenA#66#')) {
+	if (!$MySQL=@mysql_connect('localhost', 'vereficentros', 'bAllenA6##6')) {
 		$t=time();
 		while (time()<$t+10) {}
-		if (!$MySQL=@mysql_connect('localhost', 'vereficentros', 'bAllenA#66#')) {
+		if (!$MySQL=@mysql_connect('localhost', 'vereficentros', 'bAllenA6##6')) {
 		echo '<br><br><br><h3 align=center">Hay problemas de comunicaci&oacute;n con la Base de datos.</h3>';
 		echo '<h4>Por favor intente mas tarde.-</h4>';
 		exit;
