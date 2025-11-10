e36ec5e1f7da284c167c1397ab6c9409267135c5
HUGO AXEL <axeh.08@gmail.com>
2025-11-10 15:27:16 -0500

    cambio de url aapi

--- facturacion_web.php
+++ facturacion_web.php
@@ -8,7 +8,7 @@ if(isset($_POST['cmd']) && $_POST['cmd']==150){
     'rfc' => $_POST['brfc'],
     'pdf' => base64_encode(file_get_contents($_FILES['constancia']['tmp_name']))
 	);
-	$url = 'https://api.2ai.io/cfdi/rfc';
+	$url = 'https://api.facturacionweb.net/cfdi/rfc';
 
 	$ch = curl_init($url);
 
@@ -104,7 +104,7 @@ if($_POST['ajax']=='buscarRfc'){
 	if (isset($_POST['idcif'])){
 		$body['idcif'] = $_POST['idcif'];
 	}
-	$url = 'https://api.2ai.io/cfdi/rfc';
+	$url = 'https://api.facturacionweb.net/cfdi/rfc';
 
 	$ch = curl_init($url);
 
